0533775b51ef7fe740b8f9f19dedbd76b8574859
alexkuznecov <kuzniecov@mail.ru>
2016-04-08 03:24:47 +0300

    Added AnimeUpdateStarter

--- CheckerBotMaven/src/main/java/by/Main.java
+++ CheckerBotMaven/src/main/java/by/Main.java
@@ -1,9 +1,8 @@
 package by;
 
-import by.domain.SearchObject;
 import by.services.AnimeService;
 import by.services.SearchService;
-import by.util.KinogoParser;
+import by.util.AnimeUpdateStarter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,36 +18,19 @@ public class Main {
         AnimeService animeService = (AnimeService) ctx.getBean("animeService");
         SearchService searchService = (SearchService) ctx.getBean("searchService");
         while (true) {
+            AnimeUpdateStarter animeUpdateStarter = new AnimeUpdateStarter(animeService, searchService);
+            animeUpdateStarter.startUpdate();
 
-//            LOG.warn("Anime update started");
-//            SearchObject lastSearchAnime = searchService.getSearchObjectByUserId(-1);
+//            LOG.warn("Film's update started");
+//            SearchObject lastSearchFilm = searchService.getSearchObjectByUserId(-1);
 //
-//            AnimeVostParser animeVostParser = new AnimeVostParser();
-//            animeVostParser.getUpdateFromSite(lastSearchAnime.getName(), lastSearchAnime.getDate());
-//
-//            LOG.info("Reading finshed");
-//
-//            for (int i =0; i< animeVostParser.getNewAnime().size(); i++) {
-//                animeService.insertAnime(animeVostParser.getNewAnime().get(i).getName(), animeVostParser.getNewAnime().get(i).getPublicationDate(), animeVostParser.getNewAnime().get(i).getSite(), animeVostParser.getNewAnime().get(i).getNewSeries());
-//                LOG.info("Inserted" + animeVostParser.getNewAnime().get(i).getName());
-//            }
-//
-//            if (!animeVostParser.getNewAnime().isEmpty()) {
-//                int size = animeVostParser.getNewAnime().size() - 1;
-//                searchService.updateLastSearchObject(-1, animeVostParser.getNewAnime().get(size).getName(), animeVostParser.getNewAnime().get(size).getPublicationDate());
-//            }
-//            LOG.warn("Anime update finished. Updated " + animeVostParser.getNewAnime().size() + " record's");
-
-            LOG.warn("Film's update started");
-            SearchObject lastSearchFilm = searchService.getSearchObjectByUserId(-1);
-
-            KinogoParser kinogoParser = new KinogoParser();
-            kinogoParser.getUpdateFromSite(lastSearchFilm.getName());
+//            KinogoParser kinogoParser = new KinogoParser();
+//            kinogoParser.getUpdateFromSite(lastSearchFilm.getName());
 
 //            BobfilmParser bobfilmParser = new BobfilmParser();
 //            bobfilmParser.getUpdateFromSite(lastSearchFilm.getName());
 
-            LOG.info("Reading finshed");
+//            LOG.info("Reading finshed");
 
 //            for (int i =0; i< animeVostParser.getNewAnime().size(); i++) {
 //                animeService.insertAnime(animeVostParser.getNewAnime().get(i).getName(), animeVostParser.getNewAnime().get(i).getPublicationDate(), animeVostParser.getNewAnime().get(i).getSite(), animeVostParser.getNewAnime().get(i).getNewSeries());
---
+++ CheckerBotMaven/src/main/java/by/util/AnimeUpdateStarter.java
@@ -0,0 +1,52 @@
+package by.util;
+
+import by.domain.SearchObject;
+import by.services.AnimeService;
+import by.services.SearchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class AnimeUpdateStarter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AnimeUpdateStarter.class);
+
+    private AnimeService animeService;
+
+    private SearchService searchService;
+
+    public AnimeUpdateStarter(AnimeService animeService, SearchService searchService) {
+        this.animeService = animeService;
+        this.searchService = searchService;
+    }
+
+    public void startUpdate() {
+        LOG.warn("Anime update started");
+        SearchObject lastSearchAnime = searchService.getSearchObjectByUserId(-1);
+
+        AnimeVostParser animeVostParser = new AnimeVostParser();
+        try {
+            animeVostParser.getUpdateFromSite(lastSearchAnime.getName(), lastSearchAnime.getDate());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        LOG.info("Reading finshed");
+
+        for (int i = 0; i < animeVostParser.getNewAnime().size(); i++) {
+            animeService.insertAnime(animeVostParser.getNewAnime().get(i).getName(),
+                    animeVostParser.getNewAnime().get(i).getPublicationDate(), animeVostParser.getNewAnime().get(i).getSite(),
+                    animeVostParser.getNewAnime().get(i).getNewSeries());
+            LOG.info("Inserted" + animeVostParser.getNewAnime().get(i).getName());
+        }
+
+        if (!animeVostParser.getNewAnime().isEmpty()) {
+            int size = animeVostParser.getNewAnime().size() - 1;
+            searchService.updateLastSearchObject(-1, animeVostParser.getNewAnime().get(size).getName(),
+                    animeVostParser.getNewAnime().get(size).getPublicationDate());
+        }
+        LOG.warn("Anime update finished. Updated " + animeVostParser.getNewAnime().size() + " record's");
+    }
+
+}
